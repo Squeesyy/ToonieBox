893c9ef52a356ae59ed87a49b09347deb1e9741c
vguttmann <61903989+vguttmann@users.noreply.github.com>
2021-01-11 22:29:05 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # change into the Tooniebox directory. This should not be necessary and is just a failsafe
-cd /home/Pi/ToonieBox
+cd /home/pi/ToonieBox
 
 # use git to get the updates from the repo, if present.
 git pull --no-commit
