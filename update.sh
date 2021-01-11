9d112aef7c94b9868141761b4bead3fdac964248
vguttmann <61903989+vguttmann@users.noreply.github.com>
2021-01-11 20:42:04 +0100

    changed path

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # change into the Tooniebox directory. This should not be necessary and is just a failsafe
-cd ~/TooonieBox
+cd /home/Pi/ToonieBox
 
 # use git to get the updates from the repo, if present.
-git pull --no-commit
+#git pull --no-commit
 
 sudo apt update
 
